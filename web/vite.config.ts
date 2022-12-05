42da82397c2eef95d7d14b31a14cf17dee3cd1af
duetvincent <vincent@duet.digital>
2022-12-05 19:24:29 +0100

    :package: changes for netlify

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    open: "/duets",
+    open: "/",
     port: 3000
   },
   build: {
