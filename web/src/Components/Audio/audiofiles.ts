5f6afbcd062a97cae803a08f699cfae318660333
Stage @ Duet <stage@duet.digital>
2022-12-05 14:05:06 +0100

    :trumpet: join music updated to instrumental of Racoon - Tijd Verliezen

--- web/src/Components/Audio/audiofiles.ts
+++ web/src/Components/Audio/audiofiles.ts
@@ -1,4 +1,5 @@
-import jM from "../../Assets/audio/Instrumental-Racoon-LoveYouMore.webm"
+// import jM from "../../Assets/audio/Instrumental-Racoon-LoveYouMore.webm"
+import jM from "../../Assets/audio/accompaniment.mp3"
 
 import waterDripCave from "../../Assets/audio/water_drip_sfx.webm"
 
