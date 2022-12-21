bd63f14d2eb085ea181c54159e534ad5edf52ee5
duetvincent <vincent@duet.digital>
2022-12-21 17:22:50 +0100

    :fire: removed strict mode for double request sending

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -5,9 +5,9 @@ import App from "./App"
 
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
+
       <BrowserRouter>
         <App />
       </BrowserRouter>
-  </React.StrictMode>
+
 )
