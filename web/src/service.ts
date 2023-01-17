d73bab6386625deed2958509cc0cf681a022fd0b
duetvincent <vincent@duet.digital>
2023-01-17 12:35:39 +0100

    :bug: right endpoint

--- web/src/service.ts
+++ web/src/service.ts
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
-export const ENDPOINT = window.location.href.includes("localhost") ? 'localhost:5000' : 'localhost:5000';
+export const ENDPOINT = window.location.href.includes("localhost") ? 'localhost:5000' : 'duetsserver.netlify.app:5000';
 
 export const socket = io(ENDPOINT, {
   closeOnBeforeunload: false,
