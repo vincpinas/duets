27ef8de094b3611703a3b0d945cff0c73eae6dc2
duetvincent <vincent@duet.digital>
2023-02-01 18:09:45 +0100

    :bug: ENDPOINT fix

--- web/src/service.ts
+++ web/src/service.ts
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
-export const ENDPOINT = window.location.href.includes("localhost") || window.location.href.includes("127.0.0.1") ? "http://127.0.0.1:5000" : "https://duetsserver.onrender.com/";
+export const ENDPOINT = window.location.href.includes("localhost") || window.location.href.includes("127.0.0.1") ? "http://127.0.0.1:5000" : "https://duetsserver.onrender.com";
 
 export const socket = io(ENDPOINT, {
   closeOnBeforeunload: false,
